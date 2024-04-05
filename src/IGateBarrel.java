72281204a2b8e8d619fe99bffb35637e5318a37f
tiagoorietnomdias <tiagoorietnomdias@gmail.com>
2024-04-05 16:48:02 +0100

    committando

---
+++ src/IGateBarrel.java
@@ -0,0 +1,18 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.*;
+import java.util.HashMap;
+import java.util.HashSet;
+
+
+public interface IGateBarrel extends Remote {
+
+    int subscribeBarrel(IBarrel iBarrelInterface) throws RemoteException;
+    public HashMap<String, HashSet<String>> getupdatedWordMap(int barrelIDRequesting) throws RemoteException;
+    public HashMap<String, HashSet<String>> getupdatedLinkMap(int barrelIDRequesting) throws  RemoteException;
+
+    public void renewBarrelState(int barrelID, IBarrel barrel)throws RemoteException;
+
+
+}
+
